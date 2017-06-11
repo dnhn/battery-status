60032ec374aa8c5eac8bd62ca34dc1b6809b78c1
Nhan To-Doan <nhantdn@users.noreply.github.com>
2017-06-11 19:04:05 +0700

    Update server script
    Use Express Router.

--- server.js
+++ server.js
@@ -1,14 +1,22 @@
 var express = require("express"),
-    app = express();
+    app = express(),
+    router = express.Router();
 
 app.set("port", (process.env.port || 5000));
 
 app.use(express.static(__dirname + "/"));
 
-app.get("/*", function(req, res) {
+router.use(function (req, res, next) {
+  console.log("%s %s", req.method, req.originalUrl);
+  next();
+});
+
+router.get("/*", function (req, res) {
   res.sendFile("index.html", { root: __dirname });
 });
 
-app.listen(app.get("port"), function() {
+app.use("/*", router);
+
+app.listen(app.get("port"), function () {
   console.log("App is running on port", app.get("port"));
 });
